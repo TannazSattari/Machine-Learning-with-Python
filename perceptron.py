159eace877352bc502a5bfb46ff6266970a28eb2
Tannaz Sattari Tabrizi <t.sattari@gmail.com>
2017-01-19 16:49:05 -0600

    perceptron classification complete

--- perceptron.py
+++ perceptron.py
@@ -2,6 +2,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
 
 class perceptron(object):
     
@@ -30,6 +32,30 @@ class perceptron(object):
 
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',header=None)
 
+def plot_decision_region(X,Y,classifier,resolution=0.02):
+    
+    colors = ['red','blue','lightgreen','gray','cyan']
+    markers = ['s','x','o','^','v']
+
+    cmap = ListedColormap(colors[:len(np.unique(Y))])
+    
+    x1_min,x1_max = X[:,0].min()-1,X[:,0].max()+1
+    x2_min,x2_max = X[:,1].min()-1,X[:,1].max()+1
+
+    xx1,xx2=np.meshgrid(np.arange(x1_min,x1_max,resolution),np.arange(x2_min,x2_max,resolution))
+    
+    Z = classifier.predict(np.array([xx1.ravel(),xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1,xx2,Z,alpha=0.4,cmap=cmap)
+    plt.xlim(xx1.min(),xx1.max())
+    plt.ylim(xx2.min(),xx2.max())
+    
+    for idx,cl in enumerate(np.unique(Y)):
+        plt.scatter(x=X[Y == cl,0],y=X[Y==cl,1],alpha=0.8, c=cmap(idx), marker=markers[idx],label = cl)
+    
+    
+    
+
 Y = df.iloc[0:100,4].values
 Y = np.where(Y=='Iris-versicolor',-1,1)
 
@@ -47,3 +73,11 @@ ppn.fit(x,Y)
 
 plt.plot(range(1,len(ppn.errors)+1),ppn.errors,marker='o')
 plt.show()
+
+plot_decision_region(x,Y,classifier=ppn)
+plt.xlabel('SepalLength[cm]')
+plt.ylabel('petal length[cm]')
+plt.legend(loc='upper left')
+plt.show()
+
+
